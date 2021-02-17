4110f42223c0c3c8f07a19e0558818bf9e344917
Hannah Voget <hannah@voget.de>
2021-02-17 14:07:32 +0100

    created seeds and restaurantsController

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([
+  {
+    name: 'Burgers',
+    address: 'Burger Street 5',
+    phone_number: '080-87686-87987',
+    category: 'french'
+  },
+  {
+    name: 'sushi',
+    address: 'suhshu road 7',
+    phone_number: '234-23423-32434',
+    category: 'japanese'
+  },
+  {
+    name: 'pommes',
+    address: 'pomes avenue 212',
+    phone_number: '12-123124-22',
+    category: 'belgian'
+  },
+   {
+    name: 'falafel place',
+    address: 'hummus square 4',
+    phone_number: '234-234-23',
+    category: 'italian'
+  },
+   {
+    name: 'wok noodles',
+    address: 'noodle street 387',
+    phone_number: '456-5464-64',
+    category: 'chinese'
+  }
+])
